10bc738f44a1329f7b729065004d2db6981e2311
Adri Van Houdt <adri.van.houdt@gmail.com>
2013-10-30 17:14:15 +0100

    finished registratie verkoop

--- Project Deel 1/src/Main/Main.java
+++ Project Deel 1/src/Main/Main.java
@@ -1,15 +1,13 @@
 package Main;
 
-import model.Festival;
-import model.FestivalGanger;
-import model.Nummer;
-import model.TicketVerkoop;
+import model.*;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import persistence.HibernateUtil;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
@@ -34,67 +32,104 @@ public class Main {
     //start console app
     private void runConsole() {
         //init menu
-        //print menu and returns the chosen option
-        switch (getOptionMainMenu()) {
-            case 1:
-                //make sure scanner is empty
-                if (scanner.hasNext()) scanner.next();
+        int menu = getOptionMainMenu();
+        while (menu != 5) {
+            //print menu and returns the chosen option
+            switch (menu) {
+                case 1:
+                    //make sure scanner is empty
+                    if (scanner.hasNext()) scanner.next();
 
-                //TODO registratie verkoop
-                //1..n; via web
+                    //TODO registratie verkoop
+                    //1..n; via web
 
-                //maak festivalganger
-                System.out.println("Geef je naam:");
-                FestivalGanger buyer = new FestivalGanger();
-                buyer.setNaam(scanner.next());
-                session.saveOrUpdate(buyer);
+                    //maak festivalganger
+                    System.out.println("Geef je naam:");
+                    FestivalGanger buyer = new FestivalGanger();
+                    buyer.setNaam(scanner.next());
+                    session.saveOrUpdate(buyer);
 
-                //get festivals
-                Query getFestivals = session.createQuery("from Festival");
-                List festivals = getFestivals.list();
+                    //get festivals
+                    Query getFestivals = session.createQuery("from Festival");
+                    List festivals = getFestivals.list();
 
-                //kies festival
-                System.out.println("Kies je festival:");
-                for (int i = 0; i < festivals.size(); i++) {
-                    System.out.print((i + 1) + ": ");
-                    System.out.println(((Festival) festivals.get(i)).getName());
-                }
+                    //kies festival
+                    System.out.println("Kies je festival:");
+                    for (int i = 0; i < festivals.size(); i++) {
+                        System.out.print((i + 1) + ": ");
+                        System.out.println(((Festival) festivals.get(i)).getName());
+                    }
 
-                int festivalChoice = scanner.nextInt();
-                if (scanner.hasNext()) scanner.next();
+                    int festivalChoice = scanner.nextInt();
+                    if (scanner.hasNext()) scanner.next();
 
-                //maak ticketverkoop
-                TicketVerkoop sale = new TicketVerkoop();
-                sale.setFestivalGanger(buyer);
-                sale.setTimestamp(new Date());
-                sale.setType(TicketVerkoop.VerkoopsType.WEB);
-                sale.setFestival((Festival) festivals.get(festivalChoice - 1));
+                    //maak ticketverkoop
+                    TicketVerkoop sale = new TicketVerkoop();
+                    sale.setFestivalGanger(buyer);
+                    sale.setTimestamp(new Date());
+                    sale.setType(TicketVerkoop.VerkoopsType.WEB);
+                    sale.setFestival((Festival) festivals.get(festivalChoice - 1));
 
-                session.saveOrUpdate(sale);
+                    session.saveOrUpdate(sale);
 
-                //get tickettypes
-                System.out.println("Welk tickettype wil je?");
-                Query getticketTypes = session.createQuery("from TicketType ");
-                //kies tickettype
-                //maak tickets
+                    //get tickettypes
+                    System.out.println("Welk tickettype wil je?");
+                    Query getticketTypes = session.createQuery("from TicketType where naam like : festivalname");
+                    String festivalname = "%" + ((Festival) festivals.get(festivalChoice - 1)).getName() + "%";
+                    getticketTypes.setString("festivalname", festivalname);
 
+                    List ticketTypes = getticketTypes.list();
 
-                //back to menu
-                break;
-            case 2:
-                //TODO opslaan passage
-                //back to menu
-                break;
-            case 3:
-                //TODO zoeken optreden
-                //back to menu
-                break;
-            case 4:
-                //TODO zoeken festival
-                //back to menu
-                break;
+                    //kies tickettype
+                    System.out.println("Kies je tickettype:");
+
+                    for (int i = 0; i < ticketTypes.size(); i++) {
+                        System.out.print((i + 1) + ": ");
+                        System.out.println(((TicketType) ticketTypes.get(i)).getNaam());
+                    }
+
+                    int typeChoice = scanner.nextInt();
+                    if (scanner.hasNext()) scanner.next();
+                    TicketType tt = (TicketType) ticketTypes.get(typeChoice - 1);
+
+                    //kies aantal
+
+                    System.out.println("Hoeveel tickets wil je?:");
+                    int aantalTickets = scanner.nextInt();
+                    if (scanner.hasNext()) scanner.next();
+
+                    //maak tickets
+                    List<Ticket> tickets = new ArrayList<>();
+
+                    for (int i = 0; i < aantalTickets; i++) {
+                        Ticket ticket = new Ticket();
+                        ticket.setTicketType(tt);
+                        ticket.setTicketVerkoop(sale);
+                        session.saveOrUpdate(ticket);
+                        tickets.add(ticket);
+                    }
+
+                    System.out.println("Het totaal bedraagt:");
+                    System.out.println(tt.getPrijs() * aantalTickets);
+
+                    break;
+                case 2:
+                    //TODO opslaan passage
+                    break;
+                case 3:
+                    //TODO zoeken optreden
+
+                    break;
+                case 4:
+                    //TODO zoeken festival
+
+                    break;
+            }
+
+            //gives the menu again
+            menu = getOptionMainMenu();
         }
-
+        System.exit(0);
     }
 
     private int getOptionMainMenu() {
@@ -106,6 +141,7 @@ public class Main {
         System.out.println("2: Opslaan passage");
         System.out.println("3: Zoeken otreden");
         System.out.println("4: Zoeken festival");
+        System.out.println("5: Stoppen");
 
         return scanner.nextInt();
     }
