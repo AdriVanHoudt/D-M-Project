6e36e4a4e47bdab6728011f78bca6bd550ac18af
Vincent Huysmans <vinny_wijnegem@hotmail.com>
2013-11-02 22:08:00 +0100

    sender/receiver

---
+++ Project Deel 1/src/deel3/Receiver.java
@@ -0,0 +1,143 @@
+package deel3;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.hibernate.*;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.xpath.XPath;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.jms.*;
+import org.w3c.dom.Document;
+import persistence.HibernateUtil;
+
+import javax.jms.Session;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import java.io.IOException;
+import java.io.StringReader;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPathConstants;
+import java.io.File;
+import java.lang.Object;
+import java.io.Reader;
+import java.util.List;
+import javax.xml.xpath.XPathExpression;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 2/11/13
+ * Time: 18:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class Receiver {
+
+    private static org.hibernate.Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+    private static Transaction tx = session.beginTransaction();
+
+    public static void main(String[] args)throws JMSException {
+        Receiver r = new Receiver();
+        r.receiveTrackings();
+    }
+
+    public void stringToDom(String xmlSource){
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+
+        try
+        {
+            builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new InputSource(new StringReader(xmlSource)));
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(document);
+
+            StreamResult result =  new StreamResult(new File("trackings.xml"));
+            transformer.transform(source, result);
+            getTrackingsFromXml();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void receiveTrackings() throws JMSException{
+        String xmlString = "";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n<Festival>\n");
+
+        // Create a ConnectionFactory
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+        // Create a Connection to ActiceMQ
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        // Create a Session that allows you to work with activeMQ
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // Create the destination queue (or retrieve it, if it already exists)
+        Destination destination = session.createQueue("TEST.SENDRECEIVE");
+
+        // Create a MessageConsumer
+        MessageConsumer consumer = session.createConsumer(destination);
+        //consumer.setMessageListener(DeliveryMode.NON_PERSISTENT);
+
+        while (true){
+            // this call blocks until a new message arrives
+            Message message = consumer.receive();
+            TextMessage textMessage = (TextMessage) message;
+            sb.append(textMessage.getText()+"\n");
+
+            if (textMessage.getText().equals("exit")){
+                consumer.close();
+                session.close();
+                connection.close();
+                sb.append("\n</Festival>");
+                xmlString = sb.toString();
+                stringToDom(xmlString);
+                System.exit(0);
+            }
+
+        }
+
+    }
+
+    public void getTrackingsFromXml() throws JDOMException{
+
+        XPathExpression xPath = XPath.("//Tracking");
+        NodeList list= (NodeList) xPath.evaluate(openXML("trackings.xml"), XPathConstants.NODESET);
+        List<Element> trackings = xPath.selectNodes(openXML("trackings.xml"));
+
+        for(Element e : trackings){
+            session.saveOrUpdate(e);
+        }
+        tx.commit();
+    }
+
+    public static org.jdom.Document openXML(String bestaandsNaam){
+        try {
+            org.jdom.Document d = new SAXBuilder().build(bestaandsNaam);
+            //return d.getRootElement();
+            return d;
+
+        } catch (JDOMException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+
+        return null;
+    }
+
+}
+
+
---
+++ Project Deel 1/src/deel3/Sender.java
@@ -0,0 +1,111 @@
+package deel3;
+
+import model.Tracking;
+import model.Zone;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.hibernate.*;
+import persistence.HibernateUtil;
+
+import javax.jms.*;
+import javax.jms.Session;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 1/11/13
+ * Time: 17:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sender {
+    private List<Tracking> trackings = new ArrayList<>();
+    org.hibernate.Session sessionHibernate = HibernateUtil.getSessionFactory().getCurrentSession();
+    Transaction tx = sessionHibernate.beginTransaction();
+
+    public static void main(String[] args) throws JMSException {
+
+        Sender s = new Sender();
+
+        s.generateTracking();
+        s.generateTrackingQueue();
+
+    }
+
+    public void generateTracking() {
+        Query getZones = sessionHibernate.createQuery("from Zone");
+        List<Zone> zones = getZones.list();
+        int month, year, day, hourOfDay, minute, second, index;
+        Random rand = new Random();
+        Boolean isIn;
+
+        for (Zone z : zones) {
+            for (int j = 0; j < 1000; j++) {
+
+                month = rand.nextInt(11);
+                year = rand.nextInt(5) + 2011;
+                day = rand.nextInt(31);
+                hourOfDay = rand.nextInt(12) + 12;
+                minute = rand.nextInt(60);
+                second = rand.nextInt(60);
+
+                index = rand.nextInt(200);
+
+                GregorianCalendar calendar = new GregorianCalendar(year, month, day, hourOfDay, minute, second);
+
+                Date timestamp = calendar.getTime();
+
+                String formattedTimeStamp = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(timestamp);
+
+                if (j % 2 == 0) {
+                    isIn = false;
+                } else {
+                    isIn = true;
+                }
+
+                if (timestamp.before(new Date())) {
+                    Tracking tracking = new Tracking();
+                    tracking.setZone(z);
+                    tracking.setTimestamp(new Date(formattedTimeStamp));
+                    tracking.setDirection(isIn);
+                    tracking.setPolsbandId(index);
+                    trackings.add(tracking);
+                } else {
+                    //do nothing
+                }
+            }
+        }
+    }
+
+    public void generateTrackingQueue() throws JMSException {
+        // Create a ConnectionFactory
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        // Create a Connection to ActiceMQ
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        // Create a Session that allows you to work with activeMQ
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // Create the destination queue (or retrieve it, if it already exists)
+        Destination destination = session.createQueue("TEST.SENDRECEIVE");
+        // Create a MessageProducer for the Destination
+        MessageProducer producer = session.createProducer(destination);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        String zoneXML = "";
+
+        for (Tracking t : trackings) {
+          zoneXML = "<Tracking zone=\"" + t.getZone().getId() + "\" timestamp=\"" + t.getTimestamp() + "\" polsbandId=\"" + t.getPolsbandId() + "\" type=\"" + t.isDirection() + "\" />";
+          TextMessage message = session.createTextMessage(zoneXML);
+          producer.send(message);
+        }
+
+        TextMessage message = session.createTextMessage("exit");
+        producer.send(message);
+
+        producer.close();
+        session.close();
+        connection.close();
+
+        System.exit(0);
+    }
+}
