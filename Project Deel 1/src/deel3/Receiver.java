693b3cbcbc1099dd2b33587d49ea924e097932d5
Vincent Huysmans <vinny_wijnegem@hotmail.com>
2013-11-04 12:29:57 +0100

    deel 3 (volledig)

--- Project Deel 1/src/deel3/Receiver.java
+++ Project Deel 1/src/deel3/Receiver.java
@@ -14,6 +14,7 @@ import org.jdom.xpath.XPath;
 import org.xml.sax.InputSource;
 
 import javax.jms.*;
+
 import org.w3c.dom.Document;
 import persistence.HibernateUtil;
 
@@ -37,23 +38,21 @@ public class Receiver {
     private static org.hibernate.Session session = HibernateUtil.getSessionFactory().getCurrentSession();
     private static Transaction tx = session.beginTransaction();
 
-    List<Attribute> trackingZones = new ArrayList<>() ;
-    List<Attribute> trackingTimestamps = new ArrayList<>() ;
-    List<Attribute> trackingPolsbandIds = new ArrayList<>() ;
-    List<Attribute> trackingTypes = new ArrayList<>() ;
+    List<Attribute> trackingZones = new ArrayList<>();
+    List<Attribute> trackingTimestamps = new ArrayList<>();
+    List<Attribute> trackingPolsbandIds = new ArrayList<>();
+    List<Attribute> trackingTypes = new ArrayList<>();
 
-    public static void main(String[] args)throws JMSException {
+    public static void main(String[] args) throws JMSException {
         Receiver r = new Receiver();
         r.receiveTrackings();
-        r.connectToMongoDB();
     }
 
-    public void stringToDom(String xmlSource){
+    public void stringToDom(String xmlSource) {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder;
 
-        try
-        {
+        try {
             builder = factory.newDocumentBuilder();
             Document document = builder.parse(new InputSource(new StringReader(xmlSource)));
 
@@ -61,7 +60,7 @@ public class Receiver {
             Transformer transformer = transformerFactory.newTransformer();
             DOMSource source = new DOMSource(document);
 
-            StreamResult result =  new StreamResult(new File("trackings.xml"));
+            StreamResult result = new StreamResult(new File("trackings.xml"));
             transformer.transform(source, result);
             getTrackingsFromXml();
         } catch (Exception e) {
@@ -71,7 +70,7 @@ public class Receiver {
 
     }
 
-    public void receiveTrackings() throws JMSException{
+    public void receiveTrackings() throws JMSException {
         String xmlString = "";
         StringBuilder sb = new StringBuilder();
         sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n<Festival>\n");
@@ -92,13 +91,13 @@ public class Receiver {
         MessageConsumer consumer = session.createConsumer(destination);
         //consumer.setMessageListener(DeliveryMode.NON_PERSISTENT);
 
-        while (true){
+        while (true) {
             // this call blocks until a new message arrives
             Message message = consumer.receive();
             TextMessage textMessage = (TextMessage) message;
-            sb.append(textMessage.getText()+"\n");
+            sb.append(textMessage.getText() + "\n");
 
-            if (textMessage.getText().equals("exit")){
+            if (textMessage.getText().equals("exit")) {
                 consumer.close();
                 session.close();
                 connection.close();
@@ -112,7 +111,7 @@ public class Receiver {
 
     }
 
-    public void getTrackingsFromXml() throws JDOMException{
+    public void getTrackingsFromXml() throws JDOMException {
         Date d = new Date();
 
         XPath xPath = XPath.newInstance("//@zone");
@@ -127,26 +126,32 @@ public class Receiver {
         xPath = XPath.newInstance("//@type");
         trackingTypes = xPath.selectNodes(openXML("trackings.xml"));
 
-        for(int i = 0; i < trackingZones.size(); i++){
+        for (int i = 0; i < trackingZones.size(); i++) {
             Tracking tracking = new Tracking();
             tracking.setPolsbandId(Integer.parseInt(trackingPolsbandIds.get(i).getValue()));
             tracking.setDirection(Boolean.parseBoolean(trackingTypes.get(i).getValue()));
-            try{
+            try {
                 String string = trackingTimestamps.get(i).getValue();
                 d = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzzz yyyy", Locale.ENGLISH).parse(string);
-            }catch(ParseException e){
+            } catch (ParseException e) {
                 e.printStackTrace();
             }
             tracking.setTimestamp(d);
             Query getZones = session.createQuery("from Zone where id = " + Integer.parseInt(trackingZones.get(i).getValue()));
             List<Zone> zones = getZones.list();
             tracking.setZone(zones.get(0));
-          //  session.saveOrUpdate(tracking);
+            //  session.saveOrUpdate(tracking);
         }
-       // tx.commit();
+        // tx.commit();
+
+        try{
+            connectToMongoDB();
+        }catch(UnknownHostException e){
+            e.getStackTrace();
+        }
     }
 
-    public static org.jdom.Document openXML(String bestaandsNaam){
+    public static org.jdom.Document openXML(String bestaandsNaam) {
         try {
             org.jdom.Document d = new SAXBuilder().build(bestaandsNaam);
             //return d.getRootElement();
@@ -162,19 +167,35 @@ public class Receiver {
         return null;
     }
 
-    public void connectToMongoDB() throws UnknownHostException{
+    public void connectToMongoDB() throws UnknownHostException {
+
         MongoConnection mc = new MongoConnection();
         DB db = mc.mongoClient.getDB("project");
-        TreeSet<Attribute> setTrackingPolsbandIds = new TreeSet<Attribute>(trackingPolsbandIds);
+        TreeSet<Integer> setTrackingPolsbandIds = new TreeSet<>();
+
+        for(Attribute a : trackingPolsbandIds){
+            setTrackingPolsbandIds.add(Integer.parseInt(a.getValue()));
+        }
+
+        BasicDBObject tracking = new BasicDBObject();
 
         DBCollection coll = db.getCollection("project");
         //collection leegmaken
         coll.drop();
 
-        BasicDBObject tracking = new BasicDBObject("name", "trackings").
-                append("type", "database").
-                append("count", 1).
-                append("info", new BasicDBObject("x", 203).append("y", 102));
+        for (Integer i : setTrackingPolsbandIds) {
+            Query getTrackings = session.createQuery("from Tracking where PolsbandId = " + i);
+            List<Tracking> trackings = getTrackings.list();
+
+            for (Tracking t : trackings) {
+                tracking = new BasicDBObject("Timestamp", t.getTimestamp()).
+                        append("Zone naam",t.getZone().getNaam()).
+                        append("Type",t.isDirection());
+            }
+
+            BasicDBObject polsband = new BasicDBObject("PolsbandId " + i,tracking);
+            coll.insert(polsband);
+        }
 
 
     }
